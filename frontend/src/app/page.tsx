665e079cd3511f7d1eb43691203bea505549f307
Ty-Cheng5 <tsenopole@gmail.com>
2026-01-06 02:09:28 -0500

    added Manny img *no animation* and only changed the front page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,15 @@
 import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import spinningCat from "../../public/spinning-cat.gif";
+import Manny from "../../public/Manny.jpg";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <section className="flex flex-col w-dvh h-dvh items-center justify-center gap-2">
-      <Image src={spinningCat} alt="Spinning cat" />
+      <Image src={Manny} alt="Manny" className="w-32 h-32 rounded-2xl object-cover mb-8" />
+        <h1 className="font-sf text-7xl font-bold text-center">Schedule announcements<br />ahead of time.</h1>
+        <p className="text-gray-500 sm:text-xl max-w-2xl mt-5 mb-10">The easiest way to schedule future announcements.</p>
       <Button>
         <LoginLink>Enter Manny Control Panel</LoginLink>
       </Button>
