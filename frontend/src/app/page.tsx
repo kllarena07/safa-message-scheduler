4045254f74e6aea582725bb074b8574de1f1bdb0
Ty-Cheng5 <tsenopole@gmail.com>
2026-05-09 16:28:26 -0400

    fixed linter error

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,4 @@
 import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
-import spinningCat from "../../public/spinning-cat.gif";
 import Manny from "../../public/Manny.jpg";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
@@ -8,8 +7,10 @@ export default function Home() {
   return (
     <section className="flex flex-col w-dvh h-dvh items-center justify-center gap-2">
       <Image src={Manny} alt="Manny" className="w-32 h-32 rounded-2xl object-cover mb-8" />
+      <div className="flex flex-col items-center transition-opacity duration-1000">
         <h1 className="font-sf text-7xl font-bold text-center">Schedule announcements<br />ahead of time.</h1>
-        <p className="text-gray-500 sm:text-xl max-w-2xl mt-5 mb-10">The easiest way to schedule future announcements.</p>
+        <p className="text-gray-500 text-center sm:text-xl max-w-2xl mt-5 mb-10">The easiest way to schedule future announcements.</p>
+      </div>
       <Button>
         <LoginLink>Enter Manny Control Panel</LoginLink>
       </Button>
